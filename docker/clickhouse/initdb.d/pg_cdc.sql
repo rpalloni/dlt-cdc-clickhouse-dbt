3bfd886e25e81d2f689217f43ff1ae3d418e1cad
rpalloni <rpdev20@gmail.com>
2026-05-13 17:52:48 +0200

    fix cdc

--- docker/clickhouse/initdb.d/pg_cdc.sql
+++ docker/clickhouse/initdb.d/pg_cdc.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS pg_cdc
-ENGINE = MaterializedPostgresSQL('postgres-source:5432', 'pgsource', 'pgsource', 'pgsource')
+ENGINE = MaterializedPostgreSQL('postgres-source:5432', 'pgsource', 'pgsource', 'pgsource')
 SETTINGS materialized_postgresql_tables_list = 'companies,invoices';
